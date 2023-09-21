aa1c5786732dfdb397671729d363d26aeda0df24
KuAnjalii <121081810+KuAnjalii@users.noreply.github.com>
2023-09-21 15:53:49 +0530

    Create Star_pattern_1.cpp

---
+++ Star_pattern_1.cpp
@@ -0,0 +1,26 @@
+/*
+
+* * * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * 
+* * * * * * * 
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* *
+*
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+for(int i=0; i<10; i++){
+    for(int j=i; j<9; j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+return 0 ;
+}
